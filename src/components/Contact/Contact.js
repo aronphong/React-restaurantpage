f531179b9f36e24fdad411062032cdad14fbe809
aron <aronphong@gmail.com>
2020-03-20 06:26:43 -0700

    Add contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Contact.module.css';
+
+const contact = () => (
+    <div className={styles.Contact}>
+        <h1>Contact Us!</h1>
+        <p>1111 BroadWay</p>
+        <p>Sacramento, CA</p>
+        <p>(916)-111-1111</p>
+    </div>
+);
+
+export default contact;
